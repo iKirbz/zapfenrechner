b9a5fc48c7f58f406011db2477e3e6220730a7e8
iKirbz <tonipr3m@gmail.com>
2023-05-28 12:57:27 +0200

    debounce on input change

--- src/components/Zapfen.tsx
+++ src/components/Zapfen.tsx
@@ -1,13 +1,164 @@
 /* global BigInt */
 
-import React, { useState } from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
+
+import { useDebounce } from "usehooks-ts";
+
 import Line from "./Line";
 import "./Zapfen.css";
 
 export enum SYMBOLS {
-  "multiply" = "×",
-  "divide" = "÷",
-  "nothing" = "",
+  multiply = "×",
+  divide = "÷",
+  nothing = "",
+}
+
+export default function Zapfen() {
+  const [zapfenValue, setZapfenValue] = useState<string>("5");
+  const [zapfenDepth, setZapfenDepth] = useState(6);
+
+  const debouncedValue = useDebounce<string>(zapfenValue, 500);
+  const debouncedDepth = useDebounce<number>(zapfenDepth, 50);
+
+  const [lines, setlines] = useState<JSX.Element[]>([]);
+
+  const zapfenCells: JSX.Element[] = [];
+
+  useEffect(() => {
+    let currentIndex = 0;
+    let lastValue: string = zapfenValue;
+
+    for (let i = 1; i < zapfenDepth; i++) {
+      const numberPairs = getLineMultiply(lastValue, i);
+
+      zapfenCells.push(
+        <Line
+          key={currentIndex++}
+          index={currentIndex}
+          operationIndex={i + 1}
+          numberPairs={numberPairs}
+          symbol={SYMBOLS.multiply}
+          multiply={true}
+        />
+      );
+
+      const stringArr = numberPairs.map((pair) => pair[0].toString());
+
+      lastValue = stringArr.join("");
+    }
+
+    const numberPairs = getLineMultiply(lastValue, zapfenDepth);
+
+    zapfenCells.push(
+      <Line
+        key={currentIndex++}
+        index={currentIndex}
+        operationIndex={2}
+        numberPairs={numberPairs}
+        symbol={SYMBOLS.divide}
+        multiply={true}
+      />
+    );
+
+    lastValue = numberPairs.map((pair) => pair[0].toString()).join("");
+
+    for (let i = 2; i < zapfenDepth; i++) {
+      const numberPairs = getLineDivide(lastValue, i);
+
+      zapfenCells.push(
+        <Line
+          key={currentIndex++}
+          index={currentIndex}
+          operationIndex={i + 1}
+          numberPairs={numberPairs}
+          symbol={SYMBOLS.divide}
+          multiply={false}
+        />
+      );
+
+      const stringArr = numberPairs.map((pair) => pair[0].toString());
+
+      lastValue = stringArr.join("");
+      lastValue = lastValue.replaceAll("-1", "");
+    }
+
+    const lastLine = getLineDivide(lastValue, zapfenDepth);
+
+    zapfenCells.push(
+      <Line
+        key={currentIndex++}
+        index={currentIndex}
+        operationIndex={0}
+        numberPairs={lastLine}
+        symbol={SYMBOLS.nothing}
+        multiply={false}
+      />
+    );
+
+    setlines(zapfenCells);
+    console.log("HERE");
+  }, [debouncedValue, debouncedDepth]);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const numberString = event.target.value;
+
+    if (numberString === "") {
+      setZapfenValue("0");
+      return;
+    }
+
+    const trimmedNumberString = numberString.replace(/^0+/, "");
+
+    if (isNaN(parseInt(trimmedNumberString))) setZapfenValue("5");
+    else setZapfenValue(trimmedNumberString);
+  };
+
+  const handleSliderChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const numberString = event.target.value;
+
+    if (!isNaN(parseInt(numberString))) setZapfenDepth(parseInt(numberString));
+    else setZapfenDepth(0);
+  };
+
+  return (
+    <>
+      <h1>Zapfenrechner</h1>
+      <div className="zapfen">
+        <div className="input">
+          <input
+            className="number"
+            type="number"
+            value={zapfenValue.toString()}
+            onChange={handleInputChange}
+          />
+          <div className="slider">
+            <input
+              className="slider-bar"
+              type="range"
+              min="5"
+              max="50"
+              value={zapfenDepth}
+              onChange={handleSliderChange}
+            />
+            <input
+              className="slider-number"
+              type="number"
+              min="0"
+              max="999"
+              value={zapfenDepth}
+              onChange={handleSliderChange}
+            />
+          </div>
+        </div>
+
+        <div className="scroll">
+          <ul>{lines}</ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 function getLineMultiply(currentValue: string, currentDepth: number) {
@@ -73,144 +224,4 @@ function getLineDivide(currentValue: string, currentDepth: number) {
   }
 
   return numberPairs;
-}
-
-export default function Zapfen() {
-  const [zapfenValue, setZapfenValue] = useState<string>("5");
-  const [zapfenDepth, setZapfenDepth] = useState(6);
-
-  const zapfenCells: JSX.Element[] = [];
-
-  let currentIndex = 0;
-  let lastValue: string = zapfenValue;
-
-  for (let i = 1; i < zapfenDepth; i++) {
-    const numberPairs = getLineMultiply(lastValue, i);
-
-    zapfenCells.push(
-      <Line
-        key={currentIndex++}
-        index={currentIndex}
-        operationIndex={i + 1}
-        numberPairs={numberPairs}
-        symbol={SYMBOLS.multiply}
-        multiply={true}
-      />
-    );
-
-    const stringArr = numberPairs.map((pair) => pair[0].toString());
-
-    lastValue = stringArr.join("");
-  }
-
-  const numberPairs = getLineMultiply(lastValue, zapfenDepth);
-
-  zapfenCells.push(
-    <Line
-      key={currentIndex++}
-      index={currentIndex}
-      operationIndex={2}
-      numberPairs={numberPairs}
-      symbol={SYMBOLS.divide}
-      multiply={true}
-    />
-  );
-
-  lastValue = numberPairs.map((pair) => pair[0].toString()).join("");
-
-  for (let i = 2; i < zapfenDepth; i++) {
-    const numberPairs = getLineDivide(lastValue, i);
-
-    zapfenCells.push(
-      <Line
-        key={currentIndex++}
-        index={currentIndex}
-        operationIndex={i + 1}
-        numberPairs={numberPairs}
-        symbol={SYMBOLS.divide}
-        multiply={false}
-      />
-    );
-
-    const stringArr = numberPairs.map((pair) => pair[0].toString());
-
-    lastValue = stringArr.join("");
-    lastValue = lastValue.replaceAll("-1", "");
-  }
-
-  const lastLine = getLineDivide(lastValue, zapfenDepth);
-
-  zapfenCells.push(
-    <Line
-      key={currentIndex++}
-      index={currentIndex}
-      operationIndex={0}
-      numberPairs={lastLine}
-      symbol={SYMBOLS.nothing}
-      multiply={false}
-    />
-  );
-
-  const handleInputChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ): void => {
-    const numberString = event.target.value;
-
-    if (numberString === "") {
-      setZapfenValue("0");
-      return;
-    }
-
-    const trimmedNumberString = numberString.replace(/^0+/, "");
-
-    if (isNaN(parseInt(trimmedNumberString))) setZapfenValue("5");
-    else setZapfenValue(trimmedNumberString);
-  };
-
-  const handleSliderChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ): void => {
-    const numberString = event.target.value;
-
-    if (!isNaN(parseInt(numberString))) setZapfenDepth(parseInt(numberString));
-    else setZapfenDepth(0);
-  };
-
-  return (
-    <>
-      <h1>Zapfenrechner</h1>
-      <div className="zapfen">
-        <div className="input">
-          <input
-            className="number"
-            type="number"
-            value={zapfenValue.toString()}
-            onChange={handleInputChange}
-          />
-          <div className="slider">
-            <input
-              className="slider-bar"
-              type="range"
-              min="5"
-              max="50"
-              value={zapfenDepth}
-              onChange={handleSliderChange}
-            />
-            <input
-              className="slider-number"
-              type="number"
-              min="0"
-              max="999"
-              value={zapfenDepth}
-              onChange={handleSliderChange}
-            />
-          </div>
-        </div>
-
-        <div className="scroll">
-          <ul>{zapfenCells}</ul>
-        </div>
-      </div>
-    </>
-  );
 }
